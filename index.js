81f7fc243ee049a44806823532b3617071b66731
Sree Ram  S S <89720320+sreeramss@users.noreply.github.com>
2024-10-06 10:58:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app=express();
 const PORT= process.env.PORT || 8000;
 
 const corsOptions = {
-    origin:process.env.CORS_ORIGIN, // Allow only your frontend origin
+    origin:'http://localhost:5173', // Allow only your frontend origin
     credentials: true, // Allow credentials (cookies)
 };
 //middleware
